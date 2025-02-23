f2b3690c1da1c5bc529ede884f18c84e260946ae
Colin <colin.diesh@gmail.com>
2025-02-23 13:54:43 -0500

    Updates

--- storybook/assets/COVIDApollo.stories-DJ6JH2ej.js
+++
@@ -1,8 +0,0 @@
-import{c as s,T as p}from"./util-DfQSrmNZ.js";import"./_commonjsHelpers-CqkleIqs.js";const m={title:"COVID example (Apollo API)",render:a=>s(a)},e={args:{locString:"NC_045512.2:17894..28259",genome:"SARS-CoV-2",trackName:"Mature peptides",type:p.ISOFORM}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    locString: 'NC_045512.2:17894..28259',
-    genome: 'SARS-CoV-2',
-    trackName: 'Mature peptides',
-    type: TRACK_TYPE.ISOFORM
-  } satisfies ApolloArgs
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const c=["Simple"];export{e as Simple,c as __namedExportsOrder,m as default};
---
+++ storybook/assets/preview-16Lwwu7a.js
@@ -0,0 +1,1 @@
+const e={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},options:{storySort:(o,r)=>o.id===r.id?0:o.id.localeCompare(r.id,void 0,{numeric:!0})}}};export{e as default};
