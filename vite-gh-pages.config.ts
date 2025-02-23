4bb52840d4b98b3213aa8d5b7d03e2b597df1e69
Colin <colin.diesh@gmail.com>
2025-02-23 12:11:42 -0500

    Misc

--- vite-gh-pages.config.ts
+++ vite-gh-pages.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 
-// this is a 'library mode' vite config for deploying demo to github pages
 export default defineConfig({
   base: './',
+  build: {
+    outDir: './out',
+    emptyOutDir: true,
+  },
 })
